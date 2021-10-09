d94fe6f50e48d2014183943659abc12683a23c37
Aliaksandr <domestic87@bk.ru>
2021-10-09 18:40:00 +0200

    add day 11

--- src/main/java/day11/task1/Task1.java
+++ src/main/java/day11/task1/Task1.java
@@ -1,7 +1,151 @@
 package day11.task1;
 
+// Эту задачу не удалось самостоятельно сделать(((
+
 public class Task1 {
     public static void main(String[] args) {
+        Warehouse warehouse = new Warehouse();
+        Picker picker = new Picker(warehouse,0,false);
+        Courier courier = new Courier(warehouse,0,false);
+        businessProcess(picker);
+        businessProcess(courier);
+        System.out.println(warehouse);
+        System.out.println(courier);
+        System.out.println(picker);
+
+        Warehouse warehouse2 = new Warehouse();
+        Picker picker2 = new Picker(warehouse2,0,false);
+        Courier courier2 = new Courier(warehouse2,0,false);
+        businessProcess(picker2);
+        businessProcess(courier2);
+        System.out.println(warehouse2);
+        System.out.println(courier2);
+        System.out.println(picker2);
+    }
+    public static void businessProcess(Worker worker){
+        for (int i =0;i<10000;i++){
+            worker.doWork();
+        }
+        worker.bonus();
+    }
+}
+class Warehouse implements Worker{
+    private int countPickedOrders =0;
+    private int countDeliveredOrders =0;
+    public void setCountPickedOrders(int countPickedOrders) {
+        this.countPickedOrders = countPickedOrders;
+    }
+    public void setCountDeliveredOrders(int countDeliveredOrders) {
+        this.countDeliveredOrders = countDeliveredOrders;
+    }
+    @Override
+    public String toString() {
+        return "Warehouse{" +
+                "countPickedOrders=" + countPickedOrders +
+                ", countDeliveredOrders=" + countDeliveredOrders +
+                '}';}
+    public int getCountPickedOrders() {
+        return countPickedOrders;}
+
+    public int getCountDeliveredOrders() {
+        return countDeliveredOrders;}
+
+    public void sumPickedOrders(){
+        countPickedOrders++;
+    }
+    public void sumDeliveredOrders(){
+        countDeliveredOrders++;
+    }
+    @Override
+    public void doWork() {
 
     }
+    @Override
+    public void bonus() {
+
+    }
+}
+class Picker implements Worker{
+    private int salary;
+    private boolean isPayed;
+    private Warehouse wh;
+
+    public Picker(Warehouse wh, int salary, boolean isPayed) {
+        this.salary = salary;
+        this.isPayed = isPayed;
+        this.wh = wh;
+    }
+    @Override
+    public String toString() {
+        return "Picker{" +
+                "salary=" + salary +
+                ", isPayed=" + isPayed +
+                '}';}
+    public int getSalary() {
+        return salary;}
+    public boolean isPayed() {
+        return isPayed;}
+
+    @Override
+    public void doWork() {
+        salary += 80;
+        wh.sumPickedOrders();
+      }
+
+    @Override
+    public void bonus() {
+        if (wh.getCountPickedOrders() < 10000){
+            System.out.println("Бонус пока не доступен");
+        return;}
+        if (isPayed){
+            System.out.println("Бонус уже был выплачен");
+        return;}
+        salary+=70000;
+        isPayed = true;
+    }
+}
+class Courier implements Worker{
+
+    private int salary;
+    private boolean isPayed;
+    private Warehouse wh;
+
+    public Courier(Warehouse wh,int salary, boolean isPayed) {
+        this.salary = salary;
+        this.isPayed = isPayed;
+        this.wh = wh;
+    }
+    @Override
+    public String toString() {
+        return "Picker{" +
+                "salary=" + salary +
+                ", isPayed=" + isPayed +
+                '}';}
+    public int getSalary() {
+        return salary;}
+    public boolean isPayed() {
+        return isPayed;}
+
+    @Override
+    public void doWork() {
+        salary+=100;
+        wh.sumDeliveredOrders();
+    }
+
+    @Override
+    public void bonus() {
+        if (wh.getCountDeliveredOrders() < 10000){
+            System.out.println("Бонус пока не доступен");
+            return;}
+        if (isPayed){
+            System.out.println("Бонус уже был выплачен");
+            return;}
+        salary+=50000;
+        isPayed = true;
+    }
+}
+
+interface Worker {
+    void doWork();
+    void bonus();
 }
--- src/main/java/day11/task2/Task2.java
+++ src/main/java/day11/task2/Task2.java
@@ -1,7 +1,203 @@
 package day11.task2;
-
+//
 public class Task2 {
     public static void main(String[] args) {
+        Warrior warrior= new Warrior();
+        Paladin paladin = new Paladin();
+        Magician magician = new Magician();
+        Shaman shaman = new Shaman();
+        warrior.physicalAttack(paladin);
+        System.out.println(paladin);
+        paladin.physicalAttack(magician);
+        System.out.println(magician);
+        shaman.healTeammate(magician);
+        System.out.println(magician);
+        magician.magicalAttack(paladin);
+        System.out.println(paladin);
+        shaman.physicalAttack(warrior);
+        System.out.println(warrior);
+        paladin.healHimself();
+        System.out.println(paladin);
+        for (int i=0;i<5;i++){
+            warrior.physicalAttack(magician);
+            System.out.println(magician);
+        }
 
     }
 }
+
+class Warrior extends Hero implements physAttak {
+    @Override
+    public String toString() {
+        return "Warrior{" +
+                "health=" + health + '}';
+    }
+    public Warrior() {
+        health = 100;
+        physDef = 80;
+        magicDef=0;
+        physAtt=30;
+    }
+    @Override
+    public void physicalAttack(Hero hero) {
+        if (hero.physDef>0 && hero.health>MIN_HEALSE){
+        hero.health = hero.health - (this.physAtt - (hero.physDef/100)*this.physAtt);
+        if(hero.health < MIN_HEALSE)
+            {hero.health = MIN_HEALSE;}}
+        if(hero.physDef == 0 && hero.health>MIN_HEALSE) {
+            hero.health-=this.physAtt;
+        if(hero.health < MIN_HEALSE)
+        {hero.health = MIN_HEALSE;}}
+    }
+}
+
+class Paladin extends Hero implements physAttak,healer{
+    public Paladin() {
+        health = 100;
+        physDef = 50;
+        magicDef = 20;
+        physAtt = 15;
+    }
+    @Override
+    public void healHimself() {
+        if(this.health<MAX_HEALSE){
+            this.health+=25;
+            if (this.health>MAX_HEALSE)
+            {this.health=MAX_HEALSE;}}
+    }
+
+    @Override
+    public void healTeammate(Hero hero) {
+        if(hero.health<MAX_HEALSE){
+            hero.health+=10;
+            if (hero.health>MAX_HEALSE)
+            {hero.health=MAX_HEALSE;}}
+    }
+
+    @Override
+    public void physicalAttack(Hero hero) {
+        if (hero.physDef>0 && hero.health>MIN_HEALSE){
+            hero.health = hero.health - (this.physAtt - (hero.physDef/100)*this.physAtt);
+            if(hero.health < MIN_HEALSE)
+            {hero.health = MIN_HEALSE;}}
+        if(hero.physDef == 0 && hero.health>MIN_HEALSE) {
+            hero.health-=this.physAtt;
+            if(hero.health < MIN_HEALSE)
+            {hero.health = MIN_HEALSE;}}
+    }
+    @Override
+    public String toString() {
+        return "Paladin{" +
+                "health=" + health + '}';
+    }
+}
+
+class Magician extends Hero implements magicalAttak,physAttak{
+    public Magician() {
+        health = 100;
+        physDef = 0;
+        magicDef = 80;
+        physAtt = 5;
+        magicAtt = 20;
+    }
+    @Override
+    public void physicalAttack(Hero hero) {
+        if (hero.physDef>0 && hero.health>MIN_HEALSE){
+            hero.health = hero.health - (this.physAtt - (hero.physDef/100)*this.physAtt);
+            if(hero.health < MIN_HEALSE)
+            {hero.health = MIN_HEALSE;}}
+        if(hero.physDef == 0 && hero.health>MIN_HEALSE) {
+            hero.health-=this.physAtt;
+            if(hero.health < MIN_HEALSE)
+            {hero.health = MIN_HEALSE;}}
+    }
+
+    @Override
+    public void magicalAttack(Hero hero) {
+        if (hero.magicDef>0 && hero.health>MIN_HEALSE){
+            hero.health = hero.health - (this.magicAtt - (hero.magicDef/100)*this.magicAtt);
+            if(hero.health < MIN_HEALSE)
+            {hero.health = MIN_HEALSE;}}
+        else if(hero.magicDef == 0 && hero.health>MIN_HEALSE) {
+            hero.health-=this.magicAtt;
+        if(hero.health < MIN_HEALSE)
+        {hero.health = MIN_HEALSE;}}
+    }
+    @Override
+    public String toString() {
+        return "Magikan{" +
+                "health=" + health + '}';
+    }
+}
+class Shaman extends Hero implements magicalAttak,physAttak,healer{
+    public Shaman() {
+        health = 100;
+        physDef = 20;
+        magicDef = 20;
+        physAtt = 10;
+        magicAtt = 15;
+    }
+    @Override
+    public void healHimself() {
+        if(this.health<MAX_HEALSE){
+            this.health+=50;
+                if (this.health>MAX_HEALSE)
+                 {this.health=MAX_HEALSE;}}
+    }
+
+    @Override
+    public void healTeammate(Hero hero) {
+        if(hero.health<MAX_HEALSE){
+            hero.health+=30;
+                if (hero.health>MAX_HEALSE)
+                 {hero.health=MAX_HEALSE;}}
+    }
+
+    @Override
+    public void physicalAttack(Hero hero) {
+        if (hero.physDef>0 && hero.health>MIN_HEALSE){
+            hero.health = hero.health - (this.physAtt - (hero.physDef/100)*this.physAtt);
+            if(hero.health < MIN_HEALSE)
+            {hero.health = MIN_HEALSE;}}
+        if(hero.physDef == 0 && hero.health>MIN_HEALSE) {
+            hero.health-=this.physAtt;
+            if(hero.health < MIN_HEALSE)
+            {hero.health = MIN_HEALSE;}}
+    }
+
+    @Override
+    public void magicalAttack(Hero hero) {
+        if (hero.magicDef>0 && hero.health>MIN_HEALSE){
+            hero.health = hero.health - (this.magicAtt - (hero.magicDef/100)*this.magicAtt);
+            if(hero.health < MIN_HEALSE)
+            {hero.health = MIN_HEALSE;}}
+        else if(hero.magicDef == 0 && hero.health>MIN_HEALSE) {
+            hero.health-=this.magicAtt;
+            if(hero.health < MIN_HEALSE)
+            {hero.health = MIN_HEALSE;}}
+    }
+    @Override
+    public String toString() {
+        return "Shaman{" +
+                "health=" + health + '}';
+    }
+}
+abstract class Hero {
+    public double health;
+    public double physDef;
+    public double magicDef;
+    public double physAtt;
+    public double magicAtt;
+    public static final int MAX_HEALSE = 100;
+    public static final int MIN_HEALSE = 0;}
+    interface healer{
+        void healHimself();
+        void healTeammate(Hero hero);
+    }
+    interface physAttak{
+        void physicalAttack(Hero hero);
+    }
+    interface magicalAttak{
+        void magicalAttack(Hero hero);
+    }
+
