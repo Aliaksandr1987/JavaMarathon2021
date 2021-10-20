7ce603772eab19c8e9a488724148696a4378ea03
Aliaksandr <domestic87@bk.ru>
2021-10-20 12:04:53 +0200

    add day 15

---
+++ src/main/java/day15/MyException2.java
@@ -0,0 +1,8 @@
+package day15;
+
+public class MyException2 extends Exception {
+
+    public MyException2(int i) {
+        System.out.println("кол-во столбцов отличаться от трех в строке: " +(i+1));
+    }
+}
